7e82971aac06e34ec6fc84017325df155bff5d6f
pavvi17 <pj010620@gmail.com>
2025-01-06 05:58:25 +0000

    Update timestamp_script.sh

--- scripts/timestamp_script.sh
+++ scripts/timestamp_script.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Input and output file paths
-input_file="logfile.log.txt"
-output_file="logfile_no_timestamps.log"
+# Input and output file paths passed as arguments
+input_file_1="$1"
+input_file_2="$2"
 
-# Use sed to remove timestamps at the beginning of each line
-sed -E 's/^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]+Z //' "$input_file" > "$output_file"
+output_file_1="master_1_Set up job.txt"
+output_file_2="branch_1_Set up job.txt"
 
-echo "Timestamps removed. Cleaned log saved to $output_file"
+# Use sed to remove timestamps at the beginning of each line from the first input file
+sed -E 's/^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]+Z //' "$input_file_1" > "$output_file_1"
+
+# Use sed to remove timestamps from the second input file
+sed -E 's/^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]+Z //' "$input_file_2" > "$output_file_2"
+
+echo "Timestamps removed. Cleaned logs saved to $output_file_1 and $output_file_2"
